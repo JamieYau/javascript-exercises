299b5b11f7ca28461ecfcab284358a49748c0f9a
JamieYau <jamie.yau2002@me.com>
2022-08-24 16:56:00 +0100

    Fix returns ERROR with non-number parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,19 +1,19 @@
-const sumAll = function(min , max) {
-    let total = 0;
-    if (min < 0 || max < 0) {
-        return "ERROR";
-    }
-    if (min > max) {
-        const temp = min;
-        min = max;
-        max = temp;
-    }
-    for (i = min; i < max + 1; i++) {
-        total += i;
-    }
-    return total;
+const sumAll = function (min, max) {
+  let total = 0;
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) {
+    return "ERROR";
+  }
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  for (i = min; i < max + 1; i++) {
+    total += i;
+  }
+  return total;
 };
-
 
 // Do not edit below this line
 module.exports = sumAll;
