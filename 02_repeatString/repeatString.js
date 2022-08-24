294e2a0111ee11d17a79d89decc3ffc75caeddb4
JamieYau <jamie.yau2002@me.com>
2022-08-24 15:55:42 +0100

    Create repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let answer = "";
+    while (num > 0) {
+        answer += word;
+        num--;
+    }
+    return answer;
 };
 
 // Do not edit below this line
