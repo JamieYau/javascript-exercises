466bf74788e0c345cead4ab64d6366e8b26acc92
JamieYau <jamie.yau2002@me.com>
2022-09-03 00:55:43 +0100

    Create recursive factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -26,7 +26,12 @@ const power = function (base, exponent) {
   return base**exponent;
 };
 
-const factorial = function () {};
+const factorial = function (num) {
+  if (num == 0) return 1;
+  else {
+    return num * factorial(num-1);
+  }
+};
 
 // Do not edit below this line
 module.exports = {
