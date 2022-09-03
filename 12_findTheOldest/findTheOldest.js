772ce589bf49e1a81610b6d6c802c85d688e4399
JamieYau <jamie.yau2002@me.com>
2022-09-03 20:15:02 +0100

    Create function to find the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const oldest = arr.sort(function(a,b){ 
+        const currentPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return currentPerson > nextPerson ? -1 : 1;
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
