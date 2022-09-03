9c0c26e58f036cca5fa0d2559f9aacb7bced87ae
JamieYau <jamie.yau2002@me.com>
2022-09-03 01:28:30 +0100

    Fix palindrome not removing punctuation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,9 @@
 const palindromes = function (word) {
+    //remove punctuation
+    word = word.replace(/[^\w\s\']|_/g, "");
+    console.log(word);
+    
+    //reverse word
     let reverse = word.split('').reverse().join('');
     return reverse === word;
 };
