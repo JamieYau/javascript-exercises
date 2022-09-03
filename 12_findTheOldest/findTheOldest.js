c15d55965bea973e40b7501f69a9abbafcb3944d
JamieYau <jamie.yau2002@me.com>
2022-09-03 20:28:51 +0100

    Fix function to use current year if undefined

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,20 @@
-const findTheOldest = function(arr) {
-    const oldest = arr.sort(function(a,b){ 
-        const currentPerson = a.yearOfDeath - a.yearOfBirth;
-        const nextPerson = b.yearOfDeath - b.yearOfBirth;
-        return currentPerson > nextPerson ? -1 : 1;
-    });
-    return oldest[0];
+const findTheOldest = function (arr) {
+  const oldest = arr.sort(function (a, b) {
+    let currentPerson, nextPerson;
+    if (typeof a.yearOfDeath === "undefined")
+      currentPerson = new Date().getFullYear() - a.yearOfBirth;
+    else {
+      currentPerson = a.yearOfDeath - a.yearOfBirth;
+    }
+    if (typeof b.yearOfDeath === "undefined")
+      nextPerson = new Date().getFullYear() - b.yearOfBirth;
+    else {
+    nextPerson = b.yearOfDeath - b.yearOfBirth;
+    }
+    console.log (a.yearOfDeath, b.yearOfDeath)
+    return currentPerson > nextPerson ? -1 : 1;
+  });
+  return oldest[0];
 };
 
 // Do not edit below this line
